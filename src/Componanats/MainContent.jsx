3763d4ed8c5740bdfd07d473beb6600a51300abe
Ahmad M <ahmadkhaled.dev@gmail.com>
2024-03-02 16:46:50 +0200

    build api request and logic

--- src/Componanats/MainContent.jsx
+++ src/Componanats/MainContent.jsx
@@ -13,76 +13,108 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useScrollTrigger } from '@mui/material';
-
-
+import moment from 'moment';
+import "moment/dist/locale/ar-dz"
+moment.locale("ar")
 
 
 export default function MainContent() {
-    /* const [timings, setTimings] = useState({ Fajr: "", Dhuhr: "", Asr: "", Maghrib: "", Isha: '' }) */
+    const [timings, setTimings] = useState({ Fajr: "", Dhuhr: "", Asr: "", Maghrib: "", Isha: '' })
+    const [selectedCity, setSelectedCity] = useState({
+        englishCity: "Riyāḑ",
+        arabicCity: "الرياض"
+    })
+    const [today, setToday] = useState('')
+
+
     const cardsData = [
         {
             id: 1,
             prayerName: "الفجر",
-            prayerTime: 5,
+            prayerTime: timings.Fajr,
             img: '/fajr.png'
         },
         {
             id: 2,
             prayerName: "الظهر",
-            prayerTime: 12,
+            prayerTime: timings.Dhuhr,
             img: '/dhhr.png'
         },
         {
             id: 3,
             prayerName: "العصر",
-            prayerTime: 4,
+            prayerTime: timings.Asr,
             img: '/asr.png'
         },
         {
             id: 4,
             prayerName: 'المغرب',
-            prayerTime: 6,
+            prayerTime: timings.Maghrib,
             img: "/sunset.png"
         },
         {
             id: 5,
             prayerName: "العشاء",
-            prayerTime: 7,
+            prayerTime: timings.Isha,
             img: '/night.png'
         }
     ]
 
+    const objectCities = [
+        {
+            englishCity: "Makkah al Mukarramah",
+            arabicCity: "مكة المكرمة"
+        },
+        {
+            englishCity: "Ar Riyāḑ",
+            arabicCity: "الرياض"
+        },
+        {
+            englishCity: "Dammam",
+            arabicCity: "الدمام"
+        }
+    ]
+    /* Ar Riyāḑ */
 
-
-    /* useEffect((() => {
-        axios.get('https://api.aladhan.com/v1/timingsByCity?city=Dubai&country=United Arab Emirates&method=8')
+    useEffect(() => {
+        axios.get(`https://api.aladhan.com/v1/timingsByCity?country=SA&city=${selectedCity.englishCity}`)
             .then(function (response) {
                 const responseTiming = response.data.data.timings
                 setTimings(response.data.data.timings)
                 console.log(response.data.data.timings);
-            })
 
-            .catch(function (error) {
-                // handle error
-                console.log(error);
+                setToday(moment().format('MMMM Do YYYY, h:mm:ss a'))
             })
-    }), [])
- */
+    }, [selectedCity])
+
+
     const cardsDataDisplay = cardsData.map((card) => {
         return (<Prayer key={card.id} prayerName={card.prayerName} prayerTime={card.prayerTime} img={card.img} />)
     })
 
+    function handleCityChange(e) {
+        /*  const x = objectCities.find((c) => {
+             return c.englishCity == e.target.value
+         })
+  */
+        setSelectedCity(e.target.value)
+
+        /*  setSelectedCity(objectCity) */
+
+    }
 
     return (
         <div style={{ position: 'absolute', left: "50%", top: "50%", transform: 'translate(-50%,-50%)' }}>
             <Grid container>
                 <Grid xs={6}>
                     <Typography variant="h6" style={{ color: "#7F7872" }}>
-                        سبمتر 9 2023/ 4:20
+                        {today}
                     </Typography>
+
                     <Typography variant="h3" style={{ color: "white" }}>
-                        مكة المكرمة
+                        {selectedCity.arabicCity}
                     </Typography>
+
                 </Grid>
                 <Grid xs={6}>
                     <Typography variant="h6" style={{ color: "#7F7872" }}>
@@ -112,17 +144,20 @@ export default function MainContent() {
             <Stack direction="row" justifyContent="center" >
                 <FormControl style={{ width: "20%", marginTop: "30px" }}>
                     <InputLabel id="demo-simple-select-label" style={{ color: "white", fontWeight: "900" }}>المدينة</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        /*  value={age} */
-                        label="المدينة"
-                    /* onChange={handleChange} */
-                    >
-                        <MenuItem value={10}>مكة المكرمة</MenuItem>
-                        <MenuItem value={20}>الرياض</MenuItem>
-                        <MenuItem value={30}>الدمام</MenuItem>
+
+                    <Select labelId="demo-simple-select-label" id="demo-simple-select" label="المدينة" onChange={handleCityChange}>
+                        {
+                            objectCities.map((city) => {
+                                return <MenuItem value={{
+                                    englishCity: city.englishCity,
+                                    arabicCity: city.arabicCity
+                                }} >{city.arabicCity}</MenuItem>
+                            })
+                        }
                     </Select>
+
+
+
                 </FormControl>
 
             </Stack >
